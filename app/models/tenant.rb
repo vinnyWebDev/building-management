2c6d5361d0c2ebd3d3f8590a377f06e5870749ea
vinnyWebDev <vinnywebdev@gmail.com>
2023-08-08 12:49:39 +0100

    input validation added

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,2 +1,8 @@
 class Tenant < ApplicationRecord
+    #check we have input for all fields, phone and email in particular
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :apartment, presence: true
+    validates :phone, presence: true, length: { minimum: 10, maximum: 10 }
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
